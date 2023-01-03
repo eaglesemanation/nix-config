aee94ce88ceb428699bd178aa30b33394b06fdb6
Vladimir Romashchenko <eaglesemanation@gmail.com>
2023-01-03 02:46:18 -0500

    fix: Override jdtls to use 1.18.0 for better support
    jdtls had a bug that caused fidget.nvim to generate annoying errors. It
    is fixed, but nixpkgs is not up to date yet

--- home-manager/bundles/dev_envs/java.nix
+++ home-manager/bundles/dev_envs/java.nix
@@ -2,13 +2,25 @@
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.bundles.dev_env.java;
+
+  jdtls = pkgs.jdt-language-server.overrideAttrs (oldAttrs: rec {
+    version = "1.18.0";
+    timestamp = "202212011657";
+
+    src = builtins.fetchurl {
+      url =
+        "https://download.eclipse.org/jdtls/milestones/${version}/jdt-language-server-${version}-${timestamp}.tar.gz";
+      sha256 = "015sis2lc8k1jg3w7fy2nfrps7zw35fsarkz1w7bhp1d8h95r1xr";
+    };
+  });
 in {
   options.bundles.dev_env.java.enable =
     mkEnableOption "Java development environment";
 
   config = mkIf cfg.enable {
     home.packages = builtins.attrValues {
-      inherit (pkgs) openjdk gradle jdt-language-server;
+      inherit (pkgs) openjdk gradle;
+      inherit jdtls;
     };
   };
 }
