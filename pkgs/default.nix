f3957e294d153f11880c0c78eda56aa45134263b
Vladimir Romashchenko <eaglesemanation@gmail.com>
2023-08-04 20:04:40 -0400

    feat(pkgs): Add multiline arrays prettier plugin

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,4 +7,6 @@ in {
   # Wrapper that strips ANSI color codes
   conventional-pre-commit-colorless =
     callPackage ./conventional-pre-commit-colorless.nix { };
+  prettier-plugin-multiline-arrays =
+    callPackage ./prettier-plugin-multiline-arrays.nix { };
 }
