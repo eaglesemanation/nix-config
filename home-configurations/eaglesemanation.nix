6810b0ca20d42725d313613ed23ed5cbfa8e8196
Vladimir Romashchenko <eaglesemanation@gmail.com>
2025-02-25 04:04:43 -0500

    Switch to fingerprint for pub key url

--- home-configurations/eaglesemanation.nix
+++ home-configurations/eaglesemanation.nix
@@ -11,7 +11,7 @@
       email = "eaglesemanation@gmail.com";
       publicKey = builtins.fetchurl {
         name = "eaglesemanation.gpg";
-        url = "https://keys.openpgp.org/vks/v1/by-email/eaglesemanation%40gmail.com";
+        url = "https://keys.openpgp.org/vks/v1/by-fingerprint/BC8AD220FEE2118ACD6FBD24C5B9ADB07DBE5A2E";
         sha256 = "sha256:1kpr9afcazsf5gmzdsg892j7m6gnh9cwlsm1aaxgfshg8s72qbjm";
       };
     };
