a0dcb9080f66b734c2b78a5de5d825f7ed576a3a
Vladimir Romashchenko <eaglesemanation@gmail.com>
2025-03-16 00:19:50 -0400

    Modify dap-ui layout

--- nixvim/language-support.nix
+++ nixvim/language-support.nix
@@ -125,9 +125,51 @@ in
       compiler.enable = true;
 
       # Attaching a debugger to a process
-      dap-ui.enable = true;
-      dap-virtual-text.enable = true;
       dap.enable = true;
+      dap-virtual-text.enable = true;
+      dap-ui = {
+        enable = true;
+        settings = {
+          layouts = [
+            {
+              elements = [
+                {
+                  id = "scopes";
+                  size = 0.25;
+                }
+                {
+                  id = "breakpoints";
+                  size = 0.25;
+                }
+                {
+                  id = "stacks";
+                  size = 0.25;
+                }
+                {
+                  id = "watches";
+                  size = 0.25;
+                }
+              ];
+              position = "right";
+              size = 40;
+            }
+            {
+              elements = [
+                {
+                  id = "repl";
+                  size = 0.5;
+                }
+                {
+                  id = "console";
+                  size = 0.5;
+                }
+              ];
+              position = "bottom";
+              size = 10;
+            }
+          ];
+        };
+      };
 
       # Running unit tests
       neotest.enable = true;
